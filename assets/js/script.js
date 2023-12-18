8d8400a0b12e08deb885d53a5baadc50f010303e
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2023-12-18 17:19:48 -0300

    Inicio de Calculadora

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const campoValorEquip = document.getElementById('valorEquip')
+const campoValorSucata = document.getElementById('valorSucata')
+const campoVidaUtil = document.getElementById('vidaUtil')
+const campoTempoUso = document.getElementById('tempoUso')
+
+document.getElementById("calcular").addEventListener('click',function(){
+    if(campoValorEquip > 0 && campoValorSucata > 0 && campoVidaUtil > 0){}
+})
